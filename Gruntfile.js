2d2a44d0b79049b5fbcd043db541ccfcb0fe481f
basarat <basaratali@gmail.com>
2013-07-14 15:52:10 +1000

    added sample watch config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,41 @@
 module.exports = function (grunt) {
     "use strict";
 
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
     grunt.initConfig({
-        clean:{
-            test:[
-                "test/**/*.js",                
+        watch: {
+            /*
+             * watch and compile typescript to javascript
+             */
+            typescript: {
+                files: ['test/work/**/*.ts'],
+                tasks: ['ts:normal']
+            }
+        },
+
+        clean: {
+            test: [
+                "test/**/*.js",
             ]
         },
-        ts:{            
-			normal:{
-				src:["test/work/**/*.ts"],				
-				verbose: true,
-				reference: "test/" // generate a reference.ts file at this place, Automatically manages relative file paths
-			},
-			fail:{
-				src:["test/fail/*.ts"],
-				verbose: true			
-			}
-        },		
+        ts: {
+            normal: {
+                src: ["test/work/**/*.ts"],
+                verbose: true,
+                reference: "test/" // generate a reference.ts file at this place, Automatically manages relative file paths
+            },
+            fail: {
+                src: ["test/fail/*.ts"],
+                verbose: true
+            }
+        },
     });
 
-    grunt.loadTasks("tasks");    
-    grunt.loadNpmTasks("grunt-contrib-clean");    
-	grunt.registerTask("test", ["clean", "ts"]);
+    grunt.loadTasks("tasks");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.registerTask("test", ["clean", "ts"]);
     grunt.registerTask("default", ["test"]);
 
 };
