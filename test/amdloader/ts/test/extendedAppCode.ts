50c40ceb20f3fa2961a9384ae58bab8c95ff54b1
Jeff May <viralidealogue@gmail.com>
2013-12-25 14:32:03 -0500

    Added tests for external reference file amdloaders.

---
+++ test/amdloader/ts/test/extendedAppCode.ts
@@ -0,0 +1,9 @@
+/// <reference path="../app/reference.ts" />
+/// <reference path="reference.ts" />
+
+class ExtendedAppCode extends ClassA {
+    constructor() {
+        super();
+        console.log('External test class');
+    }
+}
