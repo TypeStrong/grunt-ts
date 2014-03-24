79d1ed76cb4e1ab4d4953f0e9be67e5a42d0dd2d
Basarat Syed <bas@picnicsoftware.com>
2014-03-24 18:59:53 +1100

    Breaking Change : reference file now uses EOL. Anything else was silly for some people. Feature: Only update reference file if the new contents are different

--- tasks/modules/reference.ts
+++ tasks/modules/reference.ts
@@ -6,6 +6,7 @@ import fs = require('fs');
 import grunt = require('grunt');
 
 import utils = require('./utils');
+import os = require('os');
 
 /////////////////////////////////////////////////////////////////////
 // Reference file logic
@@ -32,8 +33,10 @@ export function updateReferenceFile(files: string[], generatedFiles: string[], r
     var i;
 
     // Read the original file if it exists
+    var referenceContents = ''
     if (fs.existsSync(referenceFile)) {
-        lines = fs.readFileSync(referenceFile).toString().split('\n');
+        referenceContents = fs.readFileSync(referenceFile).toString();
+        lines = referenceContents.split(/\r\n|\r|\n/);
 
         var inSignatureSection = false;
 
@@ -92,21 +95,16 @@ export function updateReferenceFile(files: string[], generatedFiles: string[], r
     });
     contents.push(ourSignatureEnd);
 
-    // Modify the orig contents to put in our contents
     var updatedFileLines = utils.insertArrayAt(origFileLines, signatureSectionPosition, contents);
-    grunt.file.write(referenceFile, updatedFileLines.join('\n'));
+    var updatedFileContents = updatedFileLines.join(os.EOL);
 
-    // Return whether the file was changed
-    if (lines.length === updatedFileLines.length) {
-        var updated = false;
-        for (i = 0; i < lines.length; i++) {
-            if (lines[i] !== updatedFileLines[i]) {
-                updated = true;
-            }
-        }
-        return updated;
+    // Modify the orig contents to put in our contents only if changed
+    // Also Return whether the file was changed
+    if (updatedFileContents !== referenceContents) {
+        grunt.file.write(referenceFile, updatedFileContents);
+        return true;
     }
     else {
-        return true;
+        return false;
     }
 }
