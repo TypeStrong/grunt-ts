e754a6b1c2bfc0a594d25f4c62cb074680bd6a5e
basarat <basaratali@gmail.com>
2014-04-26 09:34:37 +1000

    fix tests to use new compiler (v1) generated javascript

--- test/expected/index/js/rootLevel.js
+++ test/expected/index/js/rootLevel.js
@@ -1,3 +1,5 @@
 var foo = require('./index/foo');
 var bar = require('./index/foo');
+var usage = foo;
+var usage = bar;
 //# sourceMappingURL=rootLevel.js.map
--- test/index/ts/rootLevel.ts
+++ test/index/ts/rootLevel.ts
@@ -1,2 +1,4 @@
 import foo = require('./index/foo');
 import bar = require('./index/foo');
+var usage = foo;
+var usage = bar;
