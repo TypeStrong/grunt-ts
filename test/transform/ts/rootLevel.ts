1e7f28af2192fc16b496484d2aee5f53aed792b4
Basarat Syed <bas@picnicsoftware.com>
2014-09-11 17:34:41 +1000

    chore(transform) added more tests for transforms

--- test/transform/ts/rootLevel.ts
+++ test/transform/ts/rootLevel.ts
@@ -1,12 +1,26 @@
 // TEST Process: 
-// 1 valid argument
-// 2 invalid argument
-// 3 delete a generated line
+// delete a generated lines
+//// Now compile and see if they reappear
 
+// valid argument folder with index
 ///ts:import=foo
 import foo = require('./foo/index'); ///ts:import:generated
 
+// valid argument folder without index
 ///ts:import=bar
 import c1 = require('./bar/c/c1'); ///ts:import:generated
 import c2 = require('./bar/c/c2'); ///ts:import:generated
- 
+
+// valid argument file
+///ts:import=A1
+import A1 = require('./foo/a/A1'); ///ts:import:generated
+
+// valid argument file renamed
+///ts:import=A1,a1
+import a1 = require('./foo/a/A1'); ///ts:import:generated
+
+///ts:import=nonexistent
+/// No file or directory matched name "nonexistent" ///ts:import:generated
+
+///ts:someunknowntransform
+/// Unknown transform ///ts:unknown:generated
