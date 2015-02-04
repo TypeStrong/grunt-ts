6546e10c7218aee12c79d07caccf42d5825e441a
Steve Ognibene <steve.ognibene@outlook.com>
2015-02-03 20:16:02 -0500

    grunt-ts generated via 1.4.1

--- tasks/modules/templateCache.js
+++ tasks/modules/templateCache.js
@@ -2,11 +2,8 @@
 var _ = require('lodash');
 var fs = require('fs');
 var path = require('path');
-
 var utils = require('./utils');
-
 var eol = utils.eol;
-
 /////////////////////////////////////////////////////////////////////
 // AngularJS templateCache
 ////////////////////////////////////////////////////////////////////
@@ -15,38 +12,24 @@ function generateTemplateCache(src, dest, basePath) {
     if (!src.length) {
         return;
     }
-
     // Resolve the relative path from basePath to each src file
-    var relativePaths = _.map(src, function (anHtmlFile) {
-        return 'text!' + utils.makeRelativePath(basePath, anHtmlFile);
-    });
-    var fileNames = _.map(src, function (anHtmlFile) {
-        return path.basename(anHtmlFile);
-    });
-    var fileVarialbeName = function (anHtmlFile) {
-        return anHtmlFile.split('.').join('_').split('-').join('_');
-    };
+    var relativePaths = _.map(src, function (anHtmlFile) { return 'text!' + utils.makeRelativePath(basePath, anHtmlFile); });
+    var fileNames = _.map(src, function (anHtmlFile) { return path.basename(anHtmlFile); });
+    var fileVarialbeName = function (anHtmlFile) { return anHtmlFile.split('.').join('_').split('-').join('_'); };
     var fileVariableNames = _.map(fileNames, fileVarialbeName);
-
     var templateCacheTemplate = _.template('// You must have requirejs + text plugin loaded for this to work.' + eol + 'define([<%=relativePathSection%>],function(<%=fileNameVariableSection%>){' + eol + 'angular.module("ng").run(["$templateCache",function($templateCache) {' + eol + '<%=templateCachePut%>' + eol + '}]);' + eol + '});');
-
     var relativePathSection = '"' + relativePaths.join('",' + eol + '"') + '"';
     var fileNameVariableSection = fileVariableNames.join(',' + eol);
-
     var templateCachePutTemplate = _.template('$templateCache.put("<%= fileName %>", <%=fileVariableName%>);');
-    var templateCachePut = _.map(fileNames, function (fileName) {
-        return templateCachePutTemplate({
-            fileName: fileName,
-            fileVariableName: fileVarialbeName(fileName)
-        });
-    }).join(eol);
-
+    var templateCachePut = _.map(fileNames, function (fileName) { return templateCachePutTemplate({
+        fileName: fileName,
+        fileVariableName: fileVarialbeName(fileName)
+    }); }).join(eol);
     var fileContent = templateCacheTemplate({
         relativePathSection: relativePathSection,
         fileNameVariableSection: fileNameVariableSection,
         templateCachePut: templateCachePut
     });
-
     // Early exit if new templateCache doesn't change
     if (fs.existsSync(dest)) {
         var originalContents = fs.readFileSync(dest).toString();
@@ -54,7 +37,6 @@ function generateTemplateCache(src, dest, basePath) {
             return;
         }
     }
-
     // write updated contents
     fs.writeFileSync(dest, fileContent);
 }
