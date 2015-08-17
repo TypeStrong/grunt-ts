e84bdbc611cc2e009c8d38d3007dd1bab23fbc82
Steve Ognibene <steve.ognibene@outlook.com>
2015-08-17 13:10:39 -0400

    initial optionsResolver tests working

--- test/optionsResolverTests.ts
+++ test/optionsResolverTests.ts
@@ -8,24 +8,33 @@ const config : {[name: string]: grunt.task.IMultiTask<ITargetOptions>} = {
   "minimalist": <any>{
     src: ["**/*.ts", "!node_modules/**/*.ts"]
   },
-  "hasES3SourceMaps": <any>{
+  "has ES3 and sourceMap": <any>{
     options: {
       target: 'es3',
       sourceMap: true
     }
   },
-  "hasES6AndNoSourceMaps": <any>{
+  "has ES6 and no sourceMap": <any>{
     options: {
       target: 'es6',
       sourceMap: false
     }
   },
-  "has outDir set": <any>{
+  "has outDir set to ./built": <any>{
     outDir: './built',
     options: {
       target: 'es6',
       sourceMap: false
     }
+  },
+  "has outDir set to ./myOutDir": <any>{
+    outDir: './myOutDir'
+  },
+  "has outDir set to null": <any>{
+    outDir: null
+  },
+  "has outDir set to undefined": <any>{
+    outDir: undefined
   }
 };
 
@@ -52,34 +61,56 @@ export var tests : nodeunit.ITestGroup = {
   //   }
   // },
 
+  "Warnings and Errors Tests": {
+
+  },
+
+
   "Precedence and defaults override Tests": {
-    "Minimalist config works": (test: nodeunit.Test) => {
+    "The grunt-ts defaults come through when not specified": (test: nodeunit.Test) => {
         test.expect(2);
-        let result = or.resolve(null, config["minimalist"]);
+        const result = or.resolve(null, config["minimalist"]);
         test.strictEqual(result.options.target, "es5");
         test.strictEqual(result.options.fast, "watch");
         test.done();
     },
     "Task properties should override grunt-ts defaults if not specified on the target": (test: nodeunit.Test) => {
         test.expect(2);
-        let result = or.resolve(config["has outDir set"], config["minimalist"]);
+        const result = or.resolve(config["has outDir set to ./built"], config["minimalist"]);
         test.strictEqual((config["minimalist"] as any).outDir, undefined);
         test.strictEqual(result.options.outDir, './built');
         test.done();
     },
-    "Task options should override grunt-ts defaults if not specified in the target options": (test) => {
-      test.expect(2);
-      let result = or.resolve(config["hasES6AndNoSourceMaps"], config["minimalist"]);
-      test.strictEqual(result.options.target, "es6");
-      test.strictEqual(result.options.sourceMap, false);
-      test.done();
-    },
-    "Properties specified on the target should override anything specified in the task and the grunt-ts defaults": (test) => {
-        test.ok(false);
+    "Task options should override grunt-ts defaults if not specified in the target options": (test: nodeunit.Test) => {
+        test.expect(2);
+        const result = or.resolve(config["has ES6 and no sourceMap"], config["minimalist"]);
+        test.strictEqual(result.options.target, "es6");
+        test.strictEqual(result.options.sourceMap, false);
         test.done();
     },
-    "Options target should override the task options 'options' object and the grunt-ts defaults": (test) => {
-        test.ok(false);
+    "Properties specified on the target should override anything specified in the task and the grunt-ts defaults": (test: nodeunit.Test) => {
+        test.expect(1);
+        const result = or.resolve(config["has outDir set to ./built"], config["has outDir set to ./myOutDir"]);
+        test.strictEqual(result.options.outDir, "./myOutDir");
+        test.done();
+    },
+    "Explicit null specified on the target should override anything specified in the task and the grunt-ts defaults": (test: nodeunit.Test) => {
+        test.expect(1);
+        const result = or.resolve(config["has outDir set to ./built"], config["has outDir set to null"]);
+        test.strictEqual(result.options.outDir, null);
+        test.done();
+    },
+    "Explicit undefined specified on the target should override anything specified in the task and the grunt-ts defaults": (test: nodeunit.Test) => {
+        test.expect(1);
+        const result = or.resolve(config["has outDir set to ./built"], config["has outDir set to undefined"]);
+        test.strictEqual(result.options.outDir, undefined);
+        test.done();
+    },
+    "Properties on target `options` should override the task options `options` object and the grunt-ts defaults": (test: nodeunit.Test) => {
+        test.expect(2);
+        const result = or.resolve(config["has ES6 and no sourceMap"], config["has ES3 and sourceMap"]);
+        test.strictEqual(result.options.target, "es3");
+        test.strictEqual(result.options.sourceMap, true);
         test.done();
     }
   },
