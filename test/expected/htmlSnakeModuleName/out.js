d616b1a092e8330d782126de6569bc61e2ca6181
raven <raven.demeyer@xenit.eu>
2016-01-21 01:30:30 +0100

    Add test for template and variable renaming.

---
+++ test/expected/htmlSnakeModuleName/out.js
@@ -0,0 +1,15 @@
+/* tslint:disable:max-line-length */
+var snakeModuleName;
+(function (snakeModuleName) {
+    var tpl_html_module;
+    (function (tpl_html_module) {
+        tpl_html_module.snakeModuleName_tpl_html_variable = '<div> Some content </div>    <span> some other content </span> ';
+    })(tpl_html_module = snakeModuleName.tpl_html_module || (snakeModuleName.tpl_html_module = {}));
+})(snakeModuleName || (snakeModuleName = {}));
+/// <reference path="../reference.ts"/>
+var boo = snakeModuleName.tpl_html_module.snakeModuleName_tpl_html_variable;
+//grunt-start
+/// <reference path="src/snake-module-name.tpl.html.ts" />
+/// <reference path="src/foo.ts" />
+//grunt-end 
+//# sourceMappingURL=out.js.map
